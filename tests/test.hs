5aacc87ab8c849182095563c29bb0dc166f6fde8
Sarah Walker <sarah@jellyroll.(none)>
2013-08-07 12:13:26 -0400

    correct version of test

--- tests/test.hs
+++ tests/test.hs
@@ -3,6 +3,7 @@ import qualified Codec.Picture as JP
 import qualified Codec.Picture.Types as JPTypes
 import qualified Data.Vector.Storable as VS
 import Control.Concurrent
+import Control.Monad
 
 main = do
   c <- hCreateC
@@ -11,21 +12,23 @@ main = do
   hConnect c pgrguid
   info <- hGetCamInfo c
   hStartCapture c
-  threadDelay 1000000
-  image <- hRetrieveBuffer c
-  dImage <- getDynamicImage image
-  JP.saveBmpImage "test.bmp"  dImage
-  test <- JP.readBitmap "test.bmp" 
-  case test of Left s -> print s
-               Right image -> putStrLn "success"
+  threadDelay 1000000  
+  --iterate 11 times:
+  sequence $ take 12 $ repeat ( hRetrieveBuffer c >>= \(FCImage nCol nRow vData) -> print (nCol * nRow) >> threadDelay 33000) 
+                                 --dImage <- getDynamicImage image
+ -- JP.saveBmpImage "test.bmp"  dImage
+ -- test <- JP.readBitmap "test.bmp" 
+ -- case test of Left s -> print s
+  --             Right image -> putStrLn "success"
   hStopCapture c
   hDisconnect c
   return ()
   
-    
+   {- 
 getDynamicImage :: FCImage -> IO JP.DynamicImage
 getDynamicImage (FCImage nCol nRow vData) = do
   let jCData = VS.map fromIntegral vData
   let imageJ = (JP.Image nCol nRow jCData ::JPTypes.Image JPTypes.Pixel8) 
   let dImage = JP.ImageRGB8 (JPTypes.promoteImage imageJ :: JPTypes.Image JPTypes.PixelRGB8)
   return dImage
+-}
