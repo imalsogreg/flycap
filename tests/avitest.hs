24a7173a48c39105a30ac2a808220bb5c6997751
Sarah Walker <sarah@jellyroll.(none)>
2013-08-21 14:58:13 -0400

    adding avis

--- tests/avitest.hs
+++ tests/avitest.hs
@@ -3,8 +3,6 @@ import Foreign.Ptr
 import System.FlyCap
 import Control.Monad
 
--- I think we have to delete the image each time, after we append it to our avi using destroy image in order to prevent the freezing/only going to 10
-
 main = do 
   c <- hCreateC
   pgr <- hGetCamIndex c 0
@@ -14,10 +12,9 @@ main = do
   ac <- createAVIContext --this is working
   option <- makeAVIOption 30.0
   openAVI ac "testingavi.avi" option
-  replicateM_ 20 (hRetBuff c >>= appendAVI ac)
-  {-closeAVI ac
+  replicateM_ 20  (hRetBuff c >>= \i -> appendAVI ac i >> destroyImage i)
+  closeAVI ac
   destroyAVI ac
-  -}
   hStopCapture c
   hDisconnect c
   hDestroyContext c
